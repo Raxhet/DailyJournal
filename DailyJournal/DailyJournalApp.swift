11ca5e85c36f8c0e9432da8639c8ea88819e120c
Merkulenko Ilia <imerkulenko@Ilas-MacBook-Pro.local>
2023-06-08 01:58:52 +0800

    Project created

---
+++ DailyJournal/DailyJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyJournalApp.swift
+//  DailyJournal
+//
+//  Created by Илья Меркуленко on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DailyJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
